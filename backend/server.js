7e6d7b98f1b40aef4b98aa819de828f7a539e6f3
bhole-chatoorey <ishika.singh.eee24@itbhu.ac.in>
2026-06-24 23:59:31 +0530

    Update CORS to allow Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -83,7 +83,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
   // Now it allows both 3000 and 3001, just in case!
-  origin: ["http://localhost:3000", "http://localhost:3001"], 
+  origin: ["http://localhost:3000", "http://localhost:3001","https://compile-and-conquer.vercel.app"], 
   credentials: true,
   methods: ['GET','POST','PUT','DELETE']
 }));
